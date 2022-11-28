62d8bc1d6b164e17a8093aa2524e041d4d49756c
SerebrovEV <89818121788@mail.ru>
2022-11-28 10:38:25 +0300

    Update listener

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,7 +2,10 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.User;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Autowired
     private TelegramBot telegramBot;
 
+
     @PostConstruct
     public void init() {
         telegramBot.setUpdatesListener(this);
@@ -28,9 +32,23 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            logger.info("User: {}. Text message: {}.", update.message().from().id(),update.message().text());
+            checkMessage(update);
             // Process your updates here
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void checkMessage(Update update) {
+        Message message = update.message();
+        String textMessage = update.message().text();
+        User user = update.message().from();
+        Long userId = user.id();
+        if (textMessage.equals("/start")) {
+            telegramBot.execute(new SendMessage(userId, "Hello!!"));
+        }else {
+            telegramBot.execute(new SendMessage(userId,"Bot in build"));
+        }
     }
 
 }
