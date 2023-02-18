d483fceab04e5e344e04d61bffbe36f70b77e22c
Eugene <89818121788@mail.ru>
2023-02-18 18:32:21 +0300

    Add JavaDoc

--- src/main/java/pro/sky/telegrambot/model/AnswerForUser.java
+++ src/main/java/pro/sky/telegrambot/model/AnswerForUser.java
@@ -1,18 +1,32 @@
 package pro.sky.telegrambot.model;
 
-// database for simple answer
-public class AnswerForUser {
+/**
+ * Enum со стандартными ответами бота.
+ */
+public enum AnswerForUser {
 
-    public final static String HELLO = "Hello! My name is HelperBot for busy people. I can remind you of important things." +
-            " You must send me the date, time and event:";
 
-    public final static String MISTAKE = "Uncorrected format for day, tame or event. You must write: ";
+    HELLO("Hello! My name is HelperBot for busy people. I can remind you of important things."+
+                  " You must send me the date, time and event:"),
 
-    public final static String EXAMPLE = "01.01.2001 12:34 Мероприятие";
-    public final static String START = "TGBot start work";
+    MISTAKE("Uncorrected format for day, time or event. You must write: "),
 
-    public final static String END = "TGBot end work";
+    EXAMPLE("01.01.2001 12:34 Мероприятие"),
 
-    public final static String DONE = "Event add to calendar";
+    START("TGBot start work"),
+
+    END("TGBot end work"),
+
+    DONE("Event add to calendar");
+
+    private String answer;
+
+    AnswerForUser(String answer) {
+        this.answer = answer;
+    }
+    @Override
+    public String toString() {
+        return answer;
+    }
 
 }
